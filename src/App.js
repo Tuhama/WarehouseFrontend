1d73f9c595f6624b4ebe9ad7abb71aad9b60a24f
areege44 <areegeorfali@gmail.com>
2019-02-10 13:34:11 +0200

    hiii

--- src/App.js
+++ src/App.js
@@ -1,60 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import { withRouter,Route, Switch, Redirect} from 'react-router-dom';
+import "./App.css";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import SideMenu from './components/ui/SideMenu';
+import SideMenu from "./components/ui/SideMenu";
 
 //import NewPosition from './components/indexes/position/NewPosition';
 //import NewDepartment from './components/indexes/department/NewDepartment';
 //import NewMaterial from './components/indexes/material/NewMaterial';
-import NewEmployee from './components/indexes/employee/NewEmployee';
-import NewSubMaterial from './components/indexes/subMaterial/NewSubMaterial';
-import NewMaterial from './components/indexes/material/NewMaterial';
-import NewMatReq from './components/matdelreq/NewMatReq';
-import NewIndex from './components/indexes/basicIndex/NewIndex';
+import NewEmployee from "./components/indexes/employee/NewEmployee";
+import NewSubMaterial from "./components/indexes/subMaterial/NewSubMaterial";
+import NewMaterial from "./components/indexes/material/NewMaterial";
+import NewMatReq from "./components/matdelreq/NewMatReq";
+import NewIndex from "./components/indexes/basicIndex/NewIndex";
 
-import {LocaleProvider,  Row, Col} from 'antd';
-import ar from 'antd/lib/locale-provider/ar_EG';
-
-
+import { LocaleProvider, Row, Col } from "antd";
+import ar from "antd/lib/locale-provider/ar_EG";
 
 class App extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <LocaleProvider locale={ar}>
+        {/*<Layout style={{direction: 'rtl', minHeight: '100vh'}}>*/}
+        <div style={{ direction: "rtl", height: "100vh", width: "100vw" }}>
+          <Row
+            style={{
+              textAlign: "center",
+              height: "15vh",
+              backgroundColor: "rgb(24, 144, 255)"
+            }}
+          >
+            <h2>برنامج إدارة المستودع</h2>
+          </Row>
+          <Row style={{ height: "80vh" }}>
+            <Col span={18}>
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={() => <Redirect to="/matdelreq" />}
+                />
 
-            <LocaleProvider locale={ar}>
+                <Route
+                  exact
+                  path="/positions"
+                  component={() => <NewIndex indexType="positions" />}
+                />
+                <Route
+                  exact
+                  path="/departments"
+                  component={() => <NewIndex indexType="departments" />}
+                />
+                <Route
+                  exact
+                  path="/contacts"
+                  component={() => <NewIndex indexType="contacts" />}
+                />
+                <Route exact path="/materials" component={NewMaterial} />
+                <Route
+                  exact
+                  path="/units"
+                  component={() => <NewIndex indexType="units" />}
+                />
+                <Route
+                  exact
+                  path="/users"
+                  component={() => <NewIndex indexType="users" />}
+                />
 
+                <Route exact path="/submaterials" component={NewSubMaterial} />
+                <Route exact path="/employees" component={NewEmployee} />
 
-                {/*<Layout style={{direction: 'rtl', minHeight: '100vh'}}>*/}
-                <div style={{direction: 'rtl', height: '100vh',width:'100vw'}}>
-                    <Row  style={{textAlign: 'center',height:'15vh',backgroundColor:'rgb(24, 144, 255)'}}>
-                        <h2>برنامج إدارة المستودع</h2>
-                    </Row>
-                    <Row style={{height:'80vh'}}>
+                <Route exact path="/matdelreq" component={NewMatReq} />
 
-                        <Col span={18}>
-
-                                <Switch>
-
-                                        <Route exact path="/" render={() => <Redirect to='/matdelreq'/>}/>
-
-                                        <Route exact path="/positions"
-                                               component={() => <NewIndex indexType='positions'/>}/>
-                                        <Route exact path="/departments"
-                                               component={() => <NewIndex indexType='departments'/>}/>
-                                        <Route exact path="/contacts"
-                                               component={() => <NewIndex indexType='contacts'/>}/>
-                                        <Route exact path="/materials" component={NewMaterial}/>
-
-
-                                        <Route exact path="/submaterials" component={NewSubMaterial}/>
-                                        <Route exact path="/employees" component={NewEmployee}/>
-
-
-                                        <Route exact path="/matdelreq" component={NewMatReq}/>
-
-                                        {/*    <Route exact path="/"
+                {/*    <Route exact path="/"
                                render={(props) => <PollList isAuthenticated={this.state.isAuthenticated}
                                                             currentUser={this.state.currentUser} handleLogout={this.handleLogout} {...props} />}>
                         </Route>
@@ -66,26 +85,19 @@ class App extends Component {
                         </Route>
                         <PrivateRoute authenticated={this.state.isAuthenticated} path="/poll/new" component={NewPoll} handleLogout={this.handleLogout}></PrivateRoute>
                         <Route component={NotFound}></Route>*/}
-
-
-                                </Switch>
-
-                        </Col>
-                        <Col span={6}>
-                                <SideMenu/>
-                        </Col>
-                    </Row>
-                    <Row style={{textAlign: 'center',height:'5vh'}}>
-
-                        ©2018 Created by Team
-
-                    </Row>
-                </div>
-
-
-            </LocaleProvider>
-        );
-    }
+              </Switch>
+            </Col>
+            <Col span={6}>
+              <SideMenu />
+            </Col>
+          </Row>
+          <Row style={{ textAlign: "center", height: "5vh" }}>
+            ©2018 Created by Team
+          </Row>
+        </div>
+      </LocaleProvider>
+    );
+  }
 }
 
 export default withRouter(App);
--- src/components/ui/SideMenu.js
+++ src/components/ui/SideMenu.js
@@ -1,81 +1,61 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-
-import {Link} from 'react-router-dom';
-import {Menu, Icon} from 'antd';
+import { Link } from "react-router-dom";
+import { Menu, Icon } from "antd";
 //import { withRouter } from "react-router";
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
-
 class SideMenu extends Component {
+  render() {
+    return (
+      <Menu
+        //onClick={this.handleClick}
+        // style={{width: 256}}
+        //theme="dark"
+        defaultSelectedKeys={["7"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+      >
+        <SubMenu key="sub1" title={<span />}>
+          <Menu.Item key="7">
+            <Link to="/matdelreq">طلب تسليم مواد</Link>
+          </Menu.Item>
+          <Menu.Item key="8">
+            <Link to="/internaldelivery">مذكرة تسليم</Link>
+          </Menu.Item>
+        </SubMenu>
 
-
-    render() {
-        return (
-
-            <Menu
-                //onClick={this.handleClick}
-                // style={{width: 256}}
-                //theme="dark"
-                defaultSelectedKeys={['7']}
-                defaultOpenKeys={['sub1']}
-                mode="inline"
-            >
-
-                <SubMenu key="sub1" title={<span></span>}>
-
-                    <Menu.Item key="7">
-                        <Link to="/matdelreq">
-                            طلب تسليم مواد
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="8">
-                        <Link to="/internaldelivery">
-                            مذكرة تسليم
-                        </Link>
-                    </Menu.Item>
-                </SubMenu>
-
-                <SubMenu key="sub2" title={<span>فهارس</span>}>
-                    <Menu.Item key="1">
-                        <Link to="/employees">
-                            موظفين
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Link to="/positions">
-                            مواقع وظيفية
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Link to="/departments">
-                            أقسام
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="4">
-                        <Link to="/materials">
-                            أصناف رئيسية
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="5">
-                        <Link to="/submaterials">
-                            المواد
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key="6">
-                        <Link to="/contacts">
-                            الجهات الخارجية
-                        </Link>
-                    </Menu.Item>
-                </SubMenu>
-
-            </Menu>
-
-
-        );
-    }
+        <SubMenu key="sub2" title={<span>فهارس</span>}>
+          <Menu.Item key="1">
+            <Link to="/employees">موظفين</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/positions">مواقع وظيفية</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/departments">أقسام</Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Link to="/materials">أصناف رئيسية</Link>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <Link to="/submaterials">المواد</Link>
+          </Menu.Item>
+          <Menu.Item key="6">
+            <Link to="/contacts">الجهات الخارجية</Link>
+          </Menu.Item>
+          <Menu.Item key="7">
+            <Link to="/units">الواحدات</Link>
+          </Menu.Item>
+          <Menu.Item key="8">
+            <Link to="/users">المستخدمين</Link>
+          </Menu.Item>
+        </SubMenu>
+      </Menu>
+    );
+  }
 }
 
 export default SideMenu;
